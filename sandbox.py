77ef1219f29bccfc13317b6d6a1ccc105a675214
AlexBesouro <zhukovoleksandrv@gmail.com>
2025-01-16 14:16:16 +0100

    pytest ci/cd

--- sandbox.py
+++ sandbox.py
@@ -1,33 +1,10 @@
-from app.schemas import Exercise
-
-
-def adding(num1:int, num2:int)->int:
-    return num1 + num2
-
-def subtracting(num1:int, num2:int)->int:
-    return num1 - num2
-
-def dividing(num1:int, num2:int)->float:
-    return num1 / num2
-
-def multiplying(num1:int, num2:int)->int:
-    return num1 * num2
-
-class InsufficientFunds(Exception):
-    pass
-
-
-class BankAcc():
-    def __init__(self, starting_balance=0):
-        self.balance = starting_balance
-
-    def deposit(self, amount):
-        self.balance += amount
-
-    def withdraw(self, amount):
-        if amount > self.balance:
-            raise InsufficientFunds("Insufficient money")
-        self.balance -= amount
-
-    def collect_interest(self):
-        self.balance *= 1.1
+from pydantic import BaseModel
+test_user = {"id" : 2}
+z = [{"title": "first title", "content": "first content", "owner_id": test_user['id']},
+     {"title": "first title", "content": "first content", "owner_id": 4}, ]
+class B(BaseModel):
+    title:str
+    content:str
+    owner_id: int
+a = B(**z)
+print(a.title)
